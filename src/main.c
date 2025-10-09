50524a03034556f9be8357f042c0590e60b2eaba
kaidev04 <kai@somesoftware.io>
2025-10-09 18:48:20 +0200

    Fix text position

--- src/main.c
+++ src/main.c
@@ -144,7 +144,7 @@ int main()
   
   while (1) {
     if (!start_game && !remove_start_text) {
-      draw_text(text_x - 20, text_y + 20, "Press KEY1 to start", COL_GOLD);
+      draw_text(text_x - 10, text_y + 20, "Press KEY1 to start", COL_GOLD);
     }
 
     if(start_game && !remove_start_text) {
